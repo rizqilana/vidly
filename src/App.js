ba5f2d1283f9c23fa00626c124245c4fdd6ba798
rizqilana <muhammadrizqi54321@gmail.com>
2019-04-11 11:25:17 +0700

    joi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieForm from './components/movieForm';
 import Movie from './components/movies';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
 import NavBar from './components/navBar';
+import LoginForm from './components/loginForm';
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies" component={Movie} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -29,6 +29,9 @@ const NavBar = () => {
           <NavLink className="nav-link" to="/rentals">
             Rentals
           </NavLink>
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
         </div>
       </div>
     </nav>
